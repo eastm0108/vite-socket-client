9ed44fb710a9010f8ed94b4ab307038577816e72
EastLin <a3162858@gmail.com>
2022-06-03 23:12:17 +0800

    調整格式跟新增一個更新使用者追蹤名單的 socket 事件

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,53 @@
 <template>
-  <div>
-    <button @click="sendUpdateNotification">
-      模擬送貼文
-    </button>
-    <P>{{ message }}</P>
-  </div>
+    <div>
+        <button @click="sendUpdateNotification">模擬送貼文</button>
+        <button @click="updateUserFollowers">模擬更新追蹤名單</button>
+        <P>{{ message }}</P>
+    </div>
 </template>
 
-
 <script setup>
-import { ref, computed, onUnmounted } from "vue";
-import { io } from "socket.io-client";
+import { ref, computed, onUnmounted } from 'vue';
+import { io } from 'socket.io-client';
 const host = 'http://localhost:3005';
 const token = '';
 
 const notificationCount = ref(0);
 
 const socket = io(host, {
-  path: "/socket.io/connect",
-  transports: ['websocket'],
-  autoConnect: true,                //启动自从自动连接
-  secure: true,
-  transports: ['websocket'],        // ['websocket', 'polling']
-  reconnection: true,               //启动重新连接
-  reconnectionAttempts: 5,          //最大重试连接次数
-  reconnectionDelay: 2000,          //最初尝试新的重新连接等待时间
-  reconnectionDelayMax: 10000, 
-  query: {
-    Authorization: `Bearer ${ token }`
-  }
+    path: '/socket.io/connect',
+    transports: ['websocket'],
+    autoConnect: true, //启动自从自动连接
+    secure: true,
+    transports: ['websocket'], // ['websocket', 'polling']
+    reconnection: true, //启动重新连接
+    reconnectionAttempts: 5, //最大重试连接次数
+    reconnectionDelay: 2000, //最初尝试新的重新连接等待时间
+    reconnectionDelayMax: 10000,
+    query: {
+        Authorization: `Bearer ${token}`,
+    },
 });
 
-socket.on("connect", () => {
-  socket.emit('newUser');
+socket.on('connect', () => socket.on('syncNotification', () => count()));
 
-  // 監聽通知事件
-  socket.on('syncNotification', () => count());
-});
-
-const count = () => notificationCount.value += 1;
+const count = () => (notificationCount.value += 1);
 
 const message = computed(() => {
-  return `被通知了幾次 ${ notificationCount.value }`
+    return `被通知了幾次 ${notificationCount.value}`;
 });
 
 // 模擬送貼文
 const sendUpdateNotification = async () => {
-  socket.emit('updatePost');
+    socket.emit('updatePost');
+};
+
+// 模擬更新追蹤名單
+const updateUserFollowers = () => {
+    socket.emit('updateUserFollowers', ['628fc03af680ed29550907e9']);
 };
 
 onUnmounted(() => {
-  socket.close();
-})
-
+    socket.close();
+});
 </script>
